7de04ef2618f35e2e79315e9c15bfb0b5d759a88
gisnaya <gisnaya.avisyah@gmail.com>
2021-02-16 11:54:29 +0700

    coba commit

--- app/src/main/java/com/example/fungihouse/OTPActivity.java
+++ app/src/main/java/com/example/fungihouse/OTPActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class OTPActivity extends AppCompatActivity {
-    Button btnSelesai;
+    Button btnKirim, btnSelesai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
