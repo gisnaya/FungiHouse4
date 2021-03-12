e1115940a1cea16578ae01ac0430196d1ef275c7
nasrularif30 <46752379+nasrularif30@users.noreply.github.com>
2021-03-12 08:29:14 +0700

    Update Action Bar

--- app/src/main/java/com/example/fungihouse/BerandaActivity.java
+++ app/src/main/java/com/example/fungihouse/BerandaActivity.java
@@ -6,16 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class BerandaActivity extends AppCompatActivity {
     Button btnLanjut;
+    LinearLayout lnSuhu, lnKelembapan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
 
+        lnKelembapan = findViewById(R.id.lnklb);
+        lnSuhu = findViewById(R.id.btn_suhu);
+        lnKelembapan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), KelembapanActivity.class);
+                startActivity(intent);
+            }
+        });
+        lnSuhu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SuhuActivity.class);
+                startActivity(intent);
+            }
+        });
         btnLanjut = findViewById(R.id.btn_lanjut);
         btnLanjut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +42,6 @@ public class BerandaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
