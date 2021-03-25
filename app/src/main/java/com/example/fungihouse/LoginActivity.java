45ae3667f939cdf03304e102fee811eb17080a88
gisnaya <gisnaya.avisyah@gmail.com>
2021-03-25 22:03:38 +0700

    login signup

--- app/src/main/java/com/example/fungihouse/LoginActivity.java
+++ app/src/main/java/com/example/fungihouse/LoginActivity.java
@@ -1,32 +1,72 @@
 package com.example.fungihouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
     Button btnMasuk;
     TextView txtLogin;
+    EditText mUsername;
+    ProgressBar progressBar;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mUsername   = findViewById(R.id.username);
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if ( mFirebaseUser !=null ){
+                    Toast.makeText(LoginActivity.this,"Berhasil Login",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, BerandaActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Silahkan Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
         btnMasuk = findViewById(R.id.btn_masuk);
         btnMasuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
                 Intent intent = new Intent(getApplicationContext(), BerandaActivity.class);
                 startActivity(intent);
+
+                if (username.isEmpty()) {
+                    mUsername.setError("Username Tidak Boleh Kosong");
+                    mUsername.requestFocus();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Berhasil Login", Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.VISIBLE);
             }
         });
         txtLogin = findViewById(R.id.txt_login);
@@ -37,5 +77,11 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
--- app/src/main/java/com/example/fungihouse/SignupActivity.java
+++ app/src/main/java/com/example/fungihouse/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.fungihouse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,45 +11,66 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
     Button btnSignup;
     TextView txtSignup;
-    EditText mUsername,mNohp;
-    FirebaseAuth fAuth;
+    EditText mNohp,mUsername;
     ProgressBar progressBar;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        mUsername   = findViewById(R.id.username);
+        mFirebaseAuth = FirebaseAuth.getInstance();
         mNohp       = findViewById(R.id.nohp);
-        fAuth  = FirebaseAuth.getInstance();
+        mUsername   = findViewById(R.id.username);
         progressBar = findViewById(R.id.progressBar2);
 
         btnSignup = findViewById(R.id.btn_signup);
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUsername.getText().toString().trim();
                 String nohp = mNohp.getText().toString().trim();
+                String username = mUsername.getText().toString().trim();
                 Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
                 startActivity(intent);
 
-                if (TextUtils.isEmpty(username)){
-                    mUsername.setError("Username telah digunakan.");
-                    return;
+                if (nohp.isEmpty()) {
+                    mNohp.setError("Nomor Handphone Tidak Boleh Kosong");
+                    mNohp.requestFocus();
                 }
-
-                if (TextUtils.isEmpty(nohp)){
-                    mNohp.setError("Nomor Handphone telah digunakan");
-                    return;
+                else if (username.isEmpty()) {
+                    mUsername.setError("Username Tidak Boleh Kosong");
+                    mUsername.requestFocus();
                 }
-
+                else if (nohp.isEmpty() && username.isEmpty()) {
+                    Toast.makeText(SignupActivity.this, "Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(nohp.isEmpty() && username.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(nohp, username).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(SignupActivity.this, "SignUp Gagal, Silahkan Ulangi", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(SignupActivity.this, BerandaActivity.class));
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(SignupActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                }
                 progressBar.setVisibility(View.VISIBLE);
 
             }
