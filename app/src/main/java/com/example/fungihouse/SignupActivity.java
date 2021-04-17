6d3e78fef3b7be40cd6cd2767b037b2930b9be82
gisnaya <gisnaya.avisyah@gmail.com>
2021-04-17 16:08:26 +0700

    otp

--- app/src/main/java/com/example/fungihouse/SignupActivity.java
+++ app/src/main/java/com/example/fungihouse/SignupActivity.java
@@ -1,11 +1,18 @@
 package com.example.fungihouse;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,66 +22,77 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignupActivity extends AppCompatActivity {
-    Button btnSignup;
     TextView txtSignup;
-    EditText mNohp,mUsername;
-    ProgressBar progressBar;
-    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mNohp       = findViewById(R.id.nohp);
-        mUsername   = findViewById(R.id.username);
-        progressBar = findViewById(R.id.progressBar2);
+        EditText inputMobile = findViewById(R.id.nohp);
+        Button buttonSignUp = findViewById(R.id.btn_signup);
 
-        btnSignup = findViewById(R.id.btn_signup);
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nohp = mNohp.getText().toString().trim();
-                String username = mUsername.getText().toString().trim();
-                Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
-                startActivity(intent);
-
-                if (nohp.isEmpty()) {
-                    mNohp.setError("Nomor Handphone Tidak Boleh Kosong");
-                    mNohp.requestFocus();
-                }
-                else if (username.isEmpty()) {
-                    mUsername.setError("Username Tidak Boleh Kosong");
-                    mUsername.requestFocus();
-                }
-                else if (nohp.isEmpty() && username.isEmpty()) {
-                    Toast.makeText(SignupActivity.this, "Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
-                }
-                else if (!(nohp.isEmpty() && username.isEmpty())){
-                    mFirebaseAuth.createUserWithEmailAndPassword(nohp, username).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(SignupActivity.this, "SignUp Gagal, Silahkan Ulangi", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
-                                startActivity(new Intent(SignupActivity.this, BerandaActivity.class));
-                            }
-                        }
-                    });
-                }
-                else {
-                    Toast.makeText(SignupActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                if(inputMobile.getText().toString().trim().isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Masukkan Nomor Handphone", Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 progressBar.setVisibility(View.VISIBLE);
+                buttonSignUp.setVisibility(View.INVISIBLE);
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+62" + inputMobile.getText().toString(),
+                        60,
+                        TimeUnit.SECONDS,
+                        SignupActivity.this,
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonSignUp.setVisibility(View.VISIBLE);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonSignUp.setVisibility(View.VISIBLE);
+                                Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonSignUp.setVisibility(View.VISIBLE);
+                                Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
+                                intent.putExtra("mobile", inputMobile.getText().toString());
+                                intent.putExtra("verificationId", verificationId);
+                                startActivity(intent);
+                            }
+                        }
+                );
 
             }
         });
+
+
         txtSignup = findViewById(R.id.txt_signup);
         txtSignup.setOnClickListener(new View.OnClickListener() {
             @Override
