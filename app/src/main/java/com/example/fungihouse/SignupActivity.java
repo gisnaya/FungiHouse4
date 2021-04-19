d5cb0991c46f3acafa5c15fbb7201e82f5926b5a
gisnaya <gisnaya.avisyah@gmail.com>
2021-04-19 13:49:43 +0700

    username signup

--- app/src/main/java/com/example/fungihouse/SignupActivity.java
+++ app/src/main/java/com/example/fungihouse/SignupActivity.java
@@ -1,18 +1,11 @@
 package com.example.fungihouse;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,28 +13,36 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskExecutors;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
 public class SignupActivity extends AppCompatActivity {
     TextView txtSignup;
+    private EditText username;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    UsernameInfo usernameInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username = findViewById(R.id.username);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Username");
+
+        usernameInfo = new UsernameInfo();
 
         final EditText inputMobile = findViewById(R.id.nohp);
         Button buttonSignUp = findViewById(R.id.btn_signup);
@@ -58,6 +59,13 @@ public class SignupActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
                 buttonSignUp.setVisibility(View.INVISIBLE);
 
+                String name = username.getText().toString();
+
+                if (TextUtils.isEmpty(name)){
+                    Toast.makeText(SignupActivity.this, "Masukkan Username", Toast.LENGTH_SHORT).show();
+                }else{
+                    addDataOnFirebase(name);
+                }
 
                 PhoneAuthProvider.getInstance().verifyPhoneNumber(
                         "+62" + inputMobile.getText().toString(),
@@ -89,6 +97,23 @@ public class SignupActivity extends AppCompatActivity {
                             }
                         }
                 );
+            }
+
+            private void addDataOnFirebase(String name) {
+                usernameInfo.setUsername(name);
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        databaseReference.setValue(name);
+                        Toast.makeText(SignupActivity.this, "Username ditambahkan", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(SignupActivity.this, "Username gagal ditambahkan" + error, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
             }
         });
 
