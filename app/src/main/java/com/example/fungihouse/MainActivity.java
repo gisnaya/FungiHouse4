512e264119ea28ca766dece835def7c7d61e3dfc
gisnaya <gisnaya.avisyah@gmail.com>
2021-04-20 20:52:24 +0700

    signup activity

--- app/src/main/java/com/example/fungihouse/MainActivity.java
+++ app/src/main/java/com/example/fungihouse/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnMulai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/fungihouse/OTPActivity.java
+++ app/src/main/java/com/example/fungihouse/OTPActivity.java
@@ -81,7 +81,7 @@ public class OTPActivity extends AppCompatActivity {
                                     progressBar.setVisibility(View.GONE);
                                     buttonSelesai.setVisibility(View.VISIBLE);
                                     if (task.isSuccessful()){
-                                        Intent intent = new Intent(getApplicationContext(), BerandaActivity.class);
+                                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
                                     }else {
